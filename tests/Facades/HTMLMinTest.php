b842aba3852139f84dfe7079eb810c90a78e0484
Kristaps Berzinch <kristapsberzinch@gmail.com>
2025-03-01 15:51:38 -0500

    Fix syntax issue

--- tests/Facades/HTMLMinTest.php
+++ tests/Facades/HTMLMinTest.php
@@ -31,7 +31,7 @@ class HTMLMinTest extends AbstractTestCase
      *
      * @return string
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'htmlmin';
     }
